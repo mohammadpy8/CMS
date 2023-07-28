9267ac9a6b3de4204488e34602f9b108b98f2e9f
mohammadpy8 <mohammadpy8@gmail.com>
2023-07-28 22:38:37 +0330

    header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React from "react";
 import SideBar from "./components/SideBar/SideBar";
+import Header from "./components/Header/Header";
+
+import "./App.css";
 
 function App() {
   return (
-    <SideBar />
+    <>
+      <SideBar />
+      <div className="main">
+        <Header />
+      </div>
+    </>
   );
 }
 
