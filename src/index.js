19adb7a730bc0642c9424581f4169d5432cfda52
mohammadpy8 <mohammadpy8@gmail.com>
2023-07-28 19:19:56 +0330

    customstyle

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import "./reset.css";
 import App from './App';
 import "bootstrap/dist/css/bootstrap.css";
-
+import "./custom.css";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
