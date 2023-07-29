a43b86391b2d12a333c17ecafd9d53eb232acb6d
mohammadpy8 <mohammadpy8@gmail.com>
2023-07-29 19:46:24 +0330

    finishModal

--- src/components/ProductTable/ProductTable.js
+++ src/components/ProductTable/ProductTable.js
@@ -3,10 +3,14 @@ import DeleteModal from "../DeleteModal/DeleteModal";
 import DetailsModal from "../DetailsModal/DetailsModal";
 import EditModal from "../EditModal/EditModal";
 
+import { AiOutlineDollarCircle } from "react-icons/ai";
+import { BiImage } from "react-icons/bi";
+import { TiInputChecked } from "react-icons/ti";
+import { MdTransitEnterexit } from "react-icons/md";
+
 import "./ProductTable.css";
 
 const ProductTable = () => {
-
   const [isShowModal, setIsShowModal] = useState(false);
   const [isDetailsModal, setDetailsModal] = useState(false);
   const [isEditeModal, setEditeModal] = useState(false);
@@ -66,12 +70,53 @@ const ProductTable = () => {
       {isDetailsModal && (
         <DetailsModal closeDetailsModalAction={closeDetailsModalAction} />
       )}
-      {isEditeModal &&
+      {isEditeModal && (
         <EditModal
           onClose={() => setEditeModal(false)}
           onSubmit={updateProductInfo}
-        ></EditModal>
-      }
+        >
+          <div className="edit-product-form-group">
+            <span>
+              <TiInputChecked className="edit-icon-form"/>
+            </span>
+            <input
+              type="text"
+              placeholder="عنوان جدید را وارد کنید"
+              className="edit-product-input"
+            />
+          </div>
+          <div className="edit-product-form-group">
+            <span>
+              <AiOutlineDollarCircle className="edit-icon-form"/>
+            </span>
+            <input
+              type="text"
+              placeholder="قیمت جدید را وارد کنید"
+              className="edit-product-input"
+            />
+          </div>
+          <div className="edit-product-form-group">
+            <span>
+              <MdTransitEnterexit className="edit-icon-form"/>
+            </span>
+            <input
+              type="text"
+              placeholder="موجودی جدید را وارد کنید"
+              className="edit-product-input"
+            />
+          </div>
+          <div className="edit-product-form-group">
+            <span>
+              <BiImage className="edit-icon-form"/>
+            </span>
+            <input
+              type="text"
+              placeholder="آدرس عکس جدید را وارد کنید"
+              className="edit-product-input"
+            />
+          </div>
+        </EditModal>
+      )}
     </>
   );
 };
