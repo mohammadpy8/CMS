84ed5d0797cf63de06e7bf5a332671cd5081fbf2
mohammadpy8 <mohammadpy8@gmail.com>
2023-07-29 07:52:29 +0330

    routes file

--- src/App.js
+++ src/App.js
@@ -2,31 +2,23 @@ import React from "react";
 import SideBar from "./components/SideBar/SideBar";
 import Header from "./components/Header/Header";
 
-import { Route, Routes } from "react-router-dom";
-
-import Products from "./components/Products/Products";
-import Comments from "./components/Comments/Comments";
-import Users from "./components/Users/Users";
-import Orders from "./components/Orders/Orders";
-import Offs from "./components/Offs/Offs";
+import { useRoutes } from "react-router-dom";
+import Routes from "./routes";
 
 import "./App.css";
 
 function App() {
+
+  const Router = useRoutes(Routes)
+
   return (
     <>
       <SideBar />
       <div className="main">
         <Header />
 
-        <Routes>
-          <Route path="/products" element={<Products />} />
-          <Route path="/comments" element={<Comments />} />
-          <Route path="/users" element={<Users />} />
-          <Route path="/orders" element={<Orders />} />
-          <Route path="/offs" element={<Offs />} />
-        </Routes>
-        
+        {Router}
+
       </div>
     </>
   );
