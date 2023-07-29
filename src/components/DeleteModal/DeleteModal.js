fbeac664a4f7456fe66ec0a1fae27ea8a6ca988d
mohammadpy8 <mohammadpy8@gmail.com>
2023-07-29 16:04:56 +0330

    fix-modal

--- src/components/DeleteModal/DeleteModal.js
+++ src/components/DeleteModal/DeleteModal.js
@@ -4,16 +4,17 @@ import reactDOM from "react-dom";
 import "./DeleteModal.css";
 
 const DeleteModal = () => {
-
   return reactDOM.createPortal(
+    <div className="modal-parent active">
       <div className="delete-modal">
         <h1>آیا از حذف مطمعن هستید؟</h1>
         <div className="delete-modal-btn">
           <button className="delete-btn delete-modal-accept">بله</button>
           <button className="delete-btn delete-modal-reject">خیر</button>
         </div>
-      </div>,
-      document.getElementById("modals-parent")
+      </div>
+    </div>,
+    document.getElementById("modals-parent")
   );
 };
 
