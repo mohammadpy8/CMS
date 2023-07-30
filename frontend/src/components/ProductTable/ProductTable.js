b43ef85f03005f99bd72cd569be625739889e6b2
mohammadpy8 <mohammadpy8@gmail.com>
2023-07-30 08:19:00 +0330

    get-api

--- frontend/src/components/ProductTable/ProductTable.js
+++ frontend/src/components/ProductTable/ProductTable.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import DeleteModal from "../DeleteModal/DeleteModal";
 import DetailsModal from "../DetailsModal/DetailsModal";
 import EditModal from "../EditModal/EditModal";
@@ -8,13 +8,22 @@ import { BiImage } from "react-icons/bi";
 import { TiInputChecked } from "react-icons/ti";
 import { MdTransitEnterexit } from "react-icons/md";
 
-import "./ProductTable.css"; 
+import "./ProductTable.css";
 
 const ProductTable = () => {
 
   const [isShowModal, setIsShowModal] = useState(false);
   const [isDetailsModal, setDetailsModal] = useState(false);
   const [isEditeModal, setEditeModal] = useState(false);
+  const [allProducts, setAllProducts] = useState([]);
+
+  useEffect(() => {
+
+    fetch("http://localhost:8000/api/products/")
+      .then((response) => response.json())
+      .then((products) => setAllProducts(products));
+    
+  }, []);
 
   const ModalHandler = () => setIsShowModal(true);
   const deleteModalCancelAction = () => setIsShowModal(false);
@@ -37,28 +46,41 @@ const ProductTable = () => {
         </thead>
 
         <tbody>
-          <tr className="product-table-tr">
-            <td>
-              <img src="logo192.png" alt="img" className="product-table-img" />
-            </td>
-            <td>ریکت</td>
-            <td>920000</td>
-            <td>82</td>
-            <td>
-              <button
-                className="product-table-btn"
-                onClick={ModalDetailHandler}
-              >
-                جزئیات
-              </button>
-              <button className="product-table-btn" onClick={ModalHandler}>
-                حذف
-              </button>
-              <button className="product-table-btn" onClick={ModalEditeHandler}>
-                ویرایش
-              </button>
-            </td>
-          </tr>
+          {allProducts.map((product) => {
+            const { id, title, price, colors, img, count, popularity } =
+              product;
+            return (
+              <tr className="product-table-tr" key={id}>
+                <td>
+                  <img
+                    src={img}
+                    alt="img"
+                    className="product-table-img"
+                  />
+                </td>
+                <td>{title}</td>
+                <td>{price}</td>
+                <td>{count}</td>
+                <td>
+                  <button
+                    className="product-table-btn"
+                    onClick={ModalDetailHandler}
+                  >
+                    جزئیات
+                  </button>
+                  <button className="product-table-btn" onClick={ModalHandler}>
+                    حذف
+                  </button>
+                  <button
+                    className="product-table-btn"
+                    onClick={ModalEditeHandler}
+                  >
+                    ویرایش
+                  </button>
+                </td>
+              </tr>
+            );
+          })}
         </tbody>
       </table>
 
@@ -78,7 +100,7 @@ const ProductTable = () => {
         >
           <div className="edit-product-form-group">
             <span>
-              <TiInputChecked className="edit-icon-form"/>
+              <TiInputChecked className="edit-icon-form" />
             </span>
             <input
               type="text"
@@ -88,7 +110,7 @@ const ProductTable = () => {
           </div>
           <div className="edit-product-form-group">
             <span>
-              <AiOutlineDollarCircle className="edit-icon-form"/>
+              <AiOutlineDollarCircle className="edit-icon-form" />
             </span>
             <input
               type="text"
@@ -98,7 +120,7 @@ const ProductTable = () => {
           </div>
           <div className="edit-product-form-group">
             <span>
-              <MdTransitEnterexit className="edit-icon-form"/>
+              <MdTransitEnterexit className="edit-icon-form" />
             </span>
             <input
               type="text"
@@ -108,7 +130,7 @@ const ProductTable = () => {
           </div>
           <div className="edit-product-form-group">
             <span>
-              <BiImage className="edit-icon-form"/>
+              <BiImage className="edit-icon-form" />
             </span>
             <input
               type="text"
